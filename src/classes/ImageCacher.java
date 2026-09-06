0d0044d9cabffc18787d46337cf8bf0ab14087bd
JustARandomProgram <imadragonjohann@gmail.com>
2026-09-06 12:37:38 -0400

    Moved assets outside of src

---
+++ src/classes/ImageCacher.java
@@ -0,0 +1,19 @@
+package classes;
+
+import javax.swing.ImageIcon;
+
+public class ImageCacher {
+    public static final ImageIcon none = new ImageIcon("src/assets/images/empty tile.png");
+    public static final ImageIcon one = new ImageIcon("src/assets/images/1.png");
+    public static final ImageIcon two = new ImageIcon("src/assets/images/2.png");
+    public static final ImageIcon three = new ImageIcon("src/assets/images/3.png");
+    public static final ImageIcon four = new ImageIcon("src/assets/images/4.png");
+    public static final ImageIcon five = new ImageIcon("src/assets/images/5.png");
+    public static final ImageIcon six = new ImageIcon("src/assets/images/6.png");
+    public static final ImageIcon seven = new ImageIcon("src/assets/images/7.png");
+    public static final ImageIcon eight = new ImageIcon("src/assets/images/8.png");
+    public static final ImageIcon mine = new ImageIcon("src/assets/images/mine.png");
+    public static final ImageIcon base = new ImageIcon("src/assets/images/base tile.png");
+    public static final ImageIcon flagged = new ImageIcon("src/assets/images/flagged tile.png");
+    public static final ImageIcon start = new ImageIcon("src/assets/images/start.png");
+}
--- src/classes/Tile.java
+++ src/classes/Tile.java
@@ -8,43 +8,28 @@ public class Tile {
     public boolean isMine = false;
     public boolean dug = false;
 
-    private static class Images {
-        public static final ImageIcon none = new ImageIcon("src/assets/images/empty tile.png");
-        public static final ImageIcon one = new ImageIcon("src/assets/images/1.png");
-        public static final ImageIcon two = new ImageIcon("src/assets/images/2.png");
-        public static final ImageIcon three = new ImageIcon("src/assets/images/3.png");
-        public static final ImageIcon four = new ImageIcon("src/assets/images/4.png");
-        public static final ImageIcon five = new ImageIcon("src/assets/images/5.png");
-        public static final ImageIcon six = new ImageIcon("src/assets/images/6.png");
-        public static final ImageIcon seven = new ImageIcon("src/assets/images/7.png");
-        public static final ImageIcon eight = new ImageIcon("src/assets/images/8.png");
-        public static final ImageIcon mine = new ImageIcon("src/assets/images/mine.png");
-        public static final ImageIcon base = new ImageIcon("src/assets/images/base tile.png");
-        public static final ImageIcon flagged = new ImageIcon("src/assets/images/flagged tile.png");
-    }
-
     public ImageIcon getImage() {
-        if (isMine) return Tile.Images.mine;
-        if (flagged) return Tile.Images.flagged;
-        if (dug) return Tile.Images.none; 
+        if (isMine) return ImageCacher.mine;
+        if (flagged) return ImageCacher.flagged;
+        if (dug) return ImageCacher.none; 
         switch (nearbyMines) {
             case 1:
-                return Tile.Images.one;
+                return ImageCacher.one;
             case 2:
-                return Tile.Images.two;
+                return ImageCacher.two;
             case 3:
-                return Tile.Images.three;
+                return ImageCacher.three;
             case 4:
-                return Tile.Images.four;
+                return ImageCacher.four;
             case 5:
-                return Tile.Images.five;
+                return ImageCacher.five;
             case 6:
-                return Tile.Images.six;
+                return ImageCacher.six;
             case 7:
-                return Tile.Images.seven;
+                return ImageCacher.seven;
             case 8:
-                return Tile.Images.eight;
+                return ImageCacher.eight;
         }
-        return Tile.Images.base;
+        return ImageCacher.base;
     }
 }
