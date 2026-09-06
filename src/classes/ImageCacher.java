8771b3de29d037635b5747d4bb959c6328744c7a
JustARandomProgram <imadragonjohann@gmail.com>
2026-09-06 12:44:01 -0400

    Fixed image paths

--- src/classes/ImageCacher.java
+++ src/classes/ImageCacher.java
@@ -3,17 +3,17 @@ package classes;
 import javax.swing.ImageIcon;
 
 public class ImageCacher {
-    public static final ImageIcon none = new ImageIcon("src/assets/images/empty tile.png");
-    public static final ImageIcon one = new ImageIcon("src/assets/images/1.png");
-    public static final ImageIcon two = new ImageIcon("src/assets/images/2.png");
-    public static final ImageIcon three = new ImageIcon("src/assets/images/3.png");
-    public static final ImageIcon four = new ImageIcon("src/assets/images/4.png");
-    public static final ImageIcon five = new ImageIcon("src/assets/images/5.png");
-    public static final ImageIcon six = new ImageIcon("src/assets/images/6.png");
-    public static final ImageIcon seven = new ImageIcon("src/assets/images/7.png");
-    public static final ImageIcon eight = new ImageIcon("src/assets/images/8.png");
-    public static final ImageIcon mine = new ImageIcon("src/assets/images/mine.png");
-    public static final ImageIcon base = new ImageIcon("src/assets/images/base tile.png");
-    public static final ImageIcon flagged = new ImageIcon("src/assets/images/flagged tile.png");
-    public static final ImageIcon start = new ImageIcon("src/assets/images/start.png");
+    public static final ImageIcon none = new ImageIcon("assets/images/empty tile.png");
+    public static final ImageIcon one = new ImageIcon("assets/images/1.png");
+    public static final ImageIcon two = new ImageIcon("assets/images/2.png");
+    public static final ImageIcon three = new ImageIcon("assets/images/3.png");
+    public static final ImageIcon four = new ImageIcon("assets/images/4.png");
+    public static final ImageIcon five = new ImageIcon("assets/images/5.png");
+    public static final ImageIcon six = new ImageIcon("assets/images/6.png");
+    public static final ImageIcon seven = new ImageIcon("assets/images/7.png");
+    public static final ImageIcon eight = new ImageIcon("assets/images/8.png");
+    public static final ImageIcon mine = new ImageIcon("assets/images/mine.png");
+    public static final ImageIcon base = new ImageIcon("assets/images/base tile.png");
+    public static final ImageIcon flagged = new ImageIcon("assets/images/flagged tile.png");
+    public static final ImageIcon start = new ImageIcon("assets/images/start.png");
 }
