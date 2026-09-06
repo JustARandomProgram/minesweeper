3fcf4ddbe1f40eddfbd5d666ae4299513a2607bd
JustARandomProgram <imadragonjohann@gmail.com>
2026-09-06 12:42:39 -0400

    Started working on panel gui

--- src/Window.java
+++ src/Window.java
@@ -1,16 +1,23 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.geom.Rectangle2D;
 
 public class Window extends JFrame {
     protected final int width, height;
     private Canvas canvas;
     private Timer timer;
 
-    public static class Canvas extends JPanel {
+    public class Canvas extends JPanel {
+        private Rectangle2D background = new Rectangle2D.Double(0, 0, width, height);
+        private Rectangle2D panel = new Rectangle2D.Double(0, 0, width, 100);
 
         @Override
         public void paintComponent(Graphics g) {
             Graphics2D g2d = (Graphics2D)g;
+            g2d.setColor(this.getBackground());
+            g2d.fill(background);
+            g2d.setColor(Color.GRAY);
+            g2d.fill(panel);
         }
     }
 
@@ -20,12 +27,12 @@ public class Window extends JFrame {
         setTitle("Minesweeper");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setResizable(false);
-        setLayout(getLayout());
-
+        
         canvas = new Canvas();
-        canvas.setPreferredSize(new Dimension(width, height-100));
-        canvas.setVisible(true);
+        canvas.setPreferredSize(new Dimension(width, height));
+
         add(canvas);
+
         pack();
         setLocationRelativeTo(null);
         timer = new Timer(1000/60, e -> {
