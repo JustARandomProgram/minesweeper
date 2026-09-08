d4f1dfa96a64b3fe1c378c2ac0d14d35ce75612b
JustARandomProgram <imadragonjohann@gmail.com>
2026-09-08 18:02:19 -0400

    Changed image getter logic

--- src/classes/Tile.java
+++ src/classes/Tile.java
@@ -9,26 +9,29 @@ public class Tile {
     public boolean dug = false;
 
     public ImageIcon getImage() {
-        if (isMine) return ImageCacher.mine;
+        if (isMine && dug) return ImageCacher.mine;
         if (flagged) return ImageCacher.flagged;
-        if (dug) return ImageCacher.none; 
-        switch (nearbyMines) {
-            case 1:
-                return ImageCacher.one;
-            case 2:
-                return ImageCacher.two;
-            case 3:
-                return ImageCacher.three;
-            case 4:
-                return ImageCacher.four;
-            case 5:
-                return ImageCacher.five;
-            case 6:
-                return ImageCacher.six;
-            case 7:
-                return ImageCacher.seven;
-            case 8:
-                return ImageCacher.eight;
+        if (dug) {
+            switch (nearbyMines) {
+                case 1:
+                    return ImageCacher.one;
+                case 2:
+                    return ImageCacher.two;
+                case 3:
+                    return ImageCacher.three;
+                case 4:
+                    return ImageCacher.four;
+                case 5:
+                    return ImageCacher.five;
+                case 6:
+                    return ImageCacher.six;
+                case 7:
+                    return ImageCacher.seven;
+                case 8:
+                    return ImageCacher.eight;
+                case 0:
+                    return ImageCacher.none; 
+            }
         }
         return ImageCacher.base;
     }
