08b4daacc00908cb928ae952aeffe07e124fc19e
JustARandomProgram <imadragonjohann@gmail.com>
2026-09-20 12:27:34 -0400

    updated paths

--- src/classes/FileCacher.java
+++ src/classes/FileCacher.java
@@ -18,24 +18,24 @@ public class FileCacher {
     }
 
     public static final class Images {
-        public static ImageIcon none = new ImageIcon("assets/images/empty tile.png");
-        public static ImageIcon one = new ImageIcon("assets/images/1.png");
-        public static ImageIcon two = new ImageIcon("assets/images/2.png");
-        public static ImageIcon three = new ImageIcon("assets/images/3.png");
-        public static ImageIcon four = new ImageIcon("assets/images/4.png");
-        public static ImageIcon five = new ImageIcon("assets/images/5.png");
-        public static ImageIcon six = new ImageIcon("assets/images/6.png");
-        public static ImageIcon seven = new ImageIcon("assets/images/7.png");
-        public static ImageIcon eight = new ImageIcon("assets/images/8.png");
-        public static ImageIcon mine = new ImageIcon("assets/images/mine.png");
-        public static ImageIcon base = new ImageIcon("assets/images/base tile.png");
-        public static ImageIcon flagged = new ImageIcon("assets/images/flagged tile.png");
-        public static ImageIcon happy = new ImageIcon("assets/images/happy.png");
-        public static ImageIcon sad = new ImageIcon("assets/images/sad.png");
-        public static ImageIcon cool = new ImageIcon("assets/images/cool.png");
+        public static ImageIcon none = new ImageIcon("src/assets/images/empty tile.png");
+        public static ImageIcon one = new ImageIcon("src/assets/images/1.png");
+        public static ImageIcon two = new ImageIcon("src/assets/images/2.png");
+        public static ImageIcon three = new ImageIcon("src/assets/images/3.png");
+        public static ImageIcon four = new ImageIcon("src/assets/images/4.png");
+        public static ImageIcon five = new ImageIcon("src/assets/images/5.png");
+        public static ImageIcon six = new ImageIcon("src/assets/images/6.png");
+        public static ImageIcon seven = new ImageIcon("src/assets/images/7.png");
+        public static ImageIcon eight = new ImageIcon("src/assets/images/8.png");
+        public static ImageIcon mine = new ImageIcon("src/assets/images/mine.png");
+        public static ImageIcon base = new ImageIcon("src/assets/images/base tile.png");
+        public static ImageIcon flagged = new ImageIcon("src/assets/images/flagged tile.png");
+        public static ImageIcon happy = new ImageIcon("src/assets/images/happy.png");
+        public static ImageIcon sad = new ImageIcon("src/assets/images/sad.png");
+        public static ImageIcon cool = new ImageIcon("src/assets/images/cool.png");
     }
     public static final class Fonts {
-        private static File sevenSegFontFile = new File("assets/fonts/Seven Segment.ttf");
+        private static File sevenSegFontFile = new File("src/assets/fonts/Seven Segment.ttf");
         public static Font sevenSegFont;
     }
 }
